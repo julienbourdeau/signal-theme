ba61f4535f13f7c8959937a8f96718f7e067d214
Julien Bourdeau <julien@sigerr.org>
2020-03-28 17:34:47 +0100

    Add Tailwind with purgeCss via Laravel mix

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+const mix = require('laravel-mix');
+const tailwind = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './**/*.hbs',
+  ],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+mix
+  .sass('assets/css/main.scss', 'assets/built')
+  .options({
+    processCssUrls: false,
+    postCss: [
+      tailwind('./tailwind.config.js'),
+      ...process.env.NODE_ENV === 'production'
+        ? [purgecss]
+        : []
+    ]
+  })
+  .setPublicPath('assets/built')
+;
