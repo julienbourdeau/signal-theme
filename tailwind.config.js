65417fea7fc1f8e5d996fb7219956e3511a07499
Julien Bourdeau <julien@sigerr.org>
2020-03-29 18:52:09 +0200

    Basic Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 module.exports = {
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      // lg: '960px',
+      // xl: '1280px',
+    },
+    fontFamily: {
+      'sans': ['Merriweather Sans', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      'serif': ['Merriweather', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      'mono':['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+    },
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      }},
   },
   variants: {},
   plugins: [],
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
     './**/*.hbs',
+    './assets/**/*.scss',
   ],
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
